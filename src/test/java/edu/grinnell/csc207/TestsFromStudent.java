0d128e7a592690e67780689a17930bb07dd5df40
HouckDev <houckdeveloper@gmail.com>
2024-10-03 09:07:17 -0500

    Part 1; make tests

--- src/test/java/edu/grinnell/csc207/TestsFromStudent.java
+++ src/test/java/edu/grinnell/csc207/TestsFromStudent.java
@@ -10,19 +10,52 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.junit.jupiter.api.Assertions.fail;
-
+import java.security.Key;
 import org.junit.jupiter.api.Test;
 
 /**
  * A place for you to put your own tests (beyond the shared repo).
  *
- * @author Your Name Here
+ * @author Paden Houck
  */
 public class TestsFromStudent {
-  /**
-   * A simple test.
-   */
+  
   @Test
-  public void alwaysPass() throws Exception {
-  } // alwaysPass()
+  public void houckPadenTest1() throws Exception {
+    AssociativeArray<String, String> aa = 
+        new AssociativeArray<String, String>();
+    aa.set("a", "A");
+    aa.set("b", "B");
+    aa.set("c", "C");
+    assertEquals("A", aa.get("a"), "test longer array");
+    assertEquals("B", aa.get("b"), "test longer array");
+    assertEquals("C", aa.get("c"), "test longer array");
+  }
+  @Test
+  public void houckPadenTest2() throws Exception {
+    AssociativeArray<String, String> aa = 
+        new AssociativeArray<String, String>();
+    aa.set("a", "A");
+    aa.set("b", "B");
+    aa.set("c", "C");
+    assertThrows(KeyNotFoundException.class,()-> aa.get("d"), "test invalid key");
+    assertThrows(NullKeyException.class,()-> aa.get(null), "test invalid key");
+  
+  }
+  @Test
+  public void houckPadenEdge1() throws Exception {
+    AssociativeArray<String, String> aa = 
+        new AssociativeArray<String, String>();
+    aa.set("a", "A");
+    aa.set("b", "B");
+    aa.set("c", "C");
+    aa.set("C", "c");
+    assertEquals("c", aa.get(aa.get("c")), "test self reference");
+    
+    AssociativeArray<String, AssociativeArray<String, String>> aa2 = 
+        new AssociativeArray<String, AssociativeArray<String, String>>();
+    aa2.set("a", aa);
+    assertEquals(aa, aa2.get("a"), "test nested array");
+    assertEquals("A", aa2.get("a").get("a"), "test nested array");
+  }
 } // class TestsFromSam
